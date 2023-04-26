9ee26049da63451103de5cef12a5efb621944ee4
ign13101 <nacho-13101@hotmail.com>
2023-04-26 18:53:10 +0200

    Header test 7

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -10,41 +10,42 @@
     DropdownMenu,
     DropdownItem,
   } from "sveltestrap";
-  import SvelteIcon from './SvelteIcon.svelte';
+  import SvelteIcon from "./SvelteIcon.svelte";
 </script>
 
 <header>
   <Navbar dark expand="md" container="fluid" style="background-color: #222222">
     <NavbarBrand href="/">
-      <SvelteIcon /> 
-      &nbsp; <b> Sistemas Orientados a Servicios</b></NavbarBrand>
+      <SvelteIcon />
+      &nbsp; <b> Sistemas Orientados a Servicios</b></NavbarBrand
+    >
 
     <Nav navbar>
       <NavItem>
         <NavLink href="/" class="text-white">Inicio</NavLink>
       </NavItem>
-      <!-- <Dropdown nav inNavbar>
+      <Dropdown nav inNavbar>
         <DropdownToggle nav caret class="text-white">Interfaces</DropdownToggle>
         <DropdownMenu end>
-          <DropdownItem
-            ><NavLink href="/association-stats" class="text-black"
+          <DropdownItem>
+            <!-- <NavLink href="/association-stats" class="text-black"
               >Asociaciones</NavLink
-            ></DropdownItem
-          >
+            > -->
+          </DropdownItem>
 
-          <DropdownItem
-            ><NavLink href="/phone-line-stats" class="text-black"
+          <DropdownItem>
+            <!-- <NavLink href="/phone-line-stats" class="text-black"
               >Líneas de teléfono</NavLink
-            ></DropdownItem
-          >
+            > -->
+          </DropdownItem>
 
-          <DropdownItem
-            ><NavLink href="/projection-homes-stats" class="text-black"
+          <DropdownItem>
+            <!-- <NavLink href="/projection-homes-stats" class="text-black"
               >Proyección de hogares</NavLink
-            ></DropdownItem
-          >
+            > -->
+          </DropdownItem>
         </DropdownMenu>
-      </Dropdown> -->
+      </Dropdown>
     </Nav>
   </Navbar>
 </header>
