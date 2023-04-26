51b4cfc247e2183349573df1cca8fe81285caf31
ign13101 <nacho-13101@hotmail.com>
2023-04-26 18:36:38 +0200

    Header import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script>
-    // import Header from "./Header.svelte";
+    import Header from "./Header.svelte";
     import Footer from "./Footer.svelte";
-    import { onMount } from "svelte";
+    // import { onMount } from "svelte";
 
 </script>
 
 <main>
-    <!-- <Header /> -->
+    <Header />
 
     <slot class="slot" />
 
